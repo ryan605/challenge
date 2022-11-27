0f8b60e98145f6805ee471854088a83db6bb4aab
ryan605 <langatryan26@gmail.com>
2022-11-27 18:24:23 +0300

    grade generator

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log(marks)
+function studentMarks(marks)
+{   Range (marks >=1 && marks <=100)
+    if(marks >79)
+    {
+        return `A` 
+    }
+    else if(marks >=60 && marks <=79)
+         return `B`
+         else if (marks >=49 && marks <=59)
+           return `C`
+           else if (marks >=40 && marks <=49)
+             return `D`
+             else(marks < 40)
+              return `E`
+}
+
