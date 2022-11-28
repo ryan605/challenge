94404a2c044be1a332fde693064580a718ccb312
ryan605 <langatryan26@gmail.com>
2022-11-28 09:50:20 +0300

    speed detector

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-console.log(marks)
-function studentMarks(marks)
-{   Range (marks >=1 && marks <=100)
-    if(marks >79)
-    {
-        return `A` 
+console.log(studentMarks(500))
+function studentMarks(marks){
+    if(marks >79 && marks <100){
+      
+
+      return `A` 
     }
-    else if(marks >=60 && marks <=79)
-         return `B`
-         else if (marks >=49 && marks <=59)
-           return `C`
-           else if (marks >=40 && marks <=49)
-             return `D`
-             else(marks < 40)
-              return `E`
+    else if(marks >=60 && marks <=79){
+      return `B`
+
+    }
+      
+    else if (marks >=49 && marks <=59){
+      return `C`
+
+    }
+           
+    else if (marks >=40 && marks <=49){
+      return `D`
+
+    }
+             
+    else if(marks < 40 && marks>=0){
+      return `E`
+
+    }else return 'not valid'
+              
 }
 
